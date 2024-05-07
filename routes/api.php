c94f60dce52f70e9ce75908127e736462b101576
Sachin Singh <singhsachinxyz@gmail.com>
2024-05-07 12:57:39 +0530

    soap services

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,44 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/import-employees', [ValidateDataController::class, 'postExcel'] );
 Route::get('/export-all-employees', [ValidateDataController::class, 'exportAllEmployees'] );
 Route::get('/export-dummy-employees/{id}', [ValidateDataController::class, 'exportDummyEmployees'] );
+
+
+Route::post('test/soap-enrollment', function (Illuminate\Http\Request $request) {
+
+    $xmlBodyContent =   '<Envelope xmlns="http://www.w3.org/2003/05/soap-envelope">
+                            <Header xmlns:wsa="http://www.w3.org/2005/08/addressing">
+                                <wsa:Action>http://tempuri.org/IBCService/GetEnrollmentDetails_Policy</wsa:Action>
+                            </Header>
+                            <Body>
+                                <GetEnrollmentDetails_Policy xmlns="http://tempuri.org/">
+                                    <UserName>' . $request->username . '</UserName>
+                                    <Password>' . $request->password . '</Password>
+                                    <PolicyNumber>' . $request->policy_no . '</PolicyNumber>
+                                    <StartIndex>' . $request->start_index . '</StartIndex>
+                                    <Range>' . $request->range . '</Range>
+                                </GetEnrollmentDetails_Policy>
+                            </Body>
+                        </Envelope>';
+    $url = 'https://m.fhpl.net/Bunnyconnect/BCService.svc';
+
+    // $response = Http::withHeaders([
+    //     'Content-Type'=>'application/soap+xml',
+    //     'SOAPAction'=>'http://tempuri.org/IBCService/GetEnrollmentDetailsEmployee_Agent'
+    // ])
+    // ->withBody($xmlBodyContent, "application/soap+xml")->post($url);
+    
+    $response = Http::withHeaders([
+        'Content-Type'=>'application/soap+xml',
+        'SOAPAction'=>'http://tempuri.org/IBCService/GetEnrollmentDetailsEmployee_Agent'
+    ])->send('POST', $url, ['body' => $xmlBodyContent]);
+
+    $responseBody = $response->body();
+    
+    if($response->successful() && !empty($responseBody)){
+        $responseArray = json_decode(convertXmlToArray($responseBody)['sBody']['GetEnrollmentDetails_PolicyResponse']['GetEnrollmentDetails_PolicyResult']);
+        return $responseArray;
+    }
+    else{
+        return $responseBody;
+    }
+});
